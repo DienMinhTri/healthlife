80446181911b2f0143d8031c128d1ee2946712ed
DienMinhTri <139950033+DienMinhTri@users.noreply.github.com>
2023-11-21 18:29:40 +0700

    Delete src directory

--- src/components/app/users/screens/Home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet, Text, Image, TextInput, Pressable } from "react-native";
-
-export default function Home() {
-  return (
-    <Text>Home</Text>
-  )
-}
